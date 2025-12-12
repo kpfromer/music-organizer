b55bb3b03ac6d1c2707f09aa5985c75bee082d45
Kyle Pfromer <kpfromer2@gmail.com>
2025-12-11 22:48:18 -0700

    frontend changes

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,46 @@
+import { useQuery } from "@tanstack/react-query";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { graphql } from "@/graphql";
+import { execute } from "@/lib/execute-graphql";
+import { APITester } from "../APITester";
+
+const TestQuery = graphql(`
+  query Test {
+    howdy
+  }
+`);
+
+export function Home() {
+	const { data } = useQuery({
+		queryKey: ["test"],
+		queryFn: () => execute(TestQuery),
+	});
+	console.log(data, data?.howdy);
+	return (
+		<div className="container mx-auto p-8 text-center relative z-10">
+			<h1>Test Query: {data?.howdy}</h1>
+			<div className="flex justify-center items-center gap-8 mb-8"></div>
+			<Card>
+				<CardHeader className="gap-4">
+					<CardTitle className="text-3xl font-bold">Bun + React</CardTitle>
+					<CardDescription>
+						Edit{" "}
+						<code className="rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono">
+							src/App.tsx
+						</code>{" "}
+						and save to test HMR
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<APITester />
+				</CardContent>
+			</Card>
+		</div>
+	);
+}
