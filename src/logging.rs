79fba695ca80d43b8874570a2ec4808bd1cc0f1f
Kyle Pfromer <kpfromer2@gmail.com>
2026-01-24 14:42:31 -0700

    better shutdown for tracing

--- src/logging.rs
+++ src/logging.rs
@@ -3,13 +3,14 @@ use color_eyre::eyre::Context;
 use opentelemetry::KeyValue;
 use opentelemetry_otlp::WithExportConfig;
 use opentelemetry_sdk::Resource;
+use opentelemetry_sdk::trace::SdkTracerProvider;
 use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 pub fn init_tracing(
     service_name: &str,
     jaeger_endpoint: Option<&str>,
     tracing_level: &str,
-) -> Result<()> {
+) -> Result<Option<SdkTracerProvider>> {
     let resource = Resource::builder()
         .with_attributes(vec![KeyValue::new(
             opentelemetry_semantic_conventions::resource::SERVICE_NAME,
@@ -17,7 +18,7 @@ pub fn init_tracing(
         )])
         .build();
 
-    let telemetry_layer = if let Some(jaeger_endpoint) = jaeger_endpoint {
+    let (telemetry_layer, tracer_provider) = if let Some(jaeger_endpoint) = jaeger_endpoint {
         // Initialize OTLP exporter using gRPC (Tonic)
         let otlp_exporter = opentelemetry_otlp::SpanExporter::builder()
             .with_tonic()
@@ -32,18 +33,22 @@ pub fn init_tracing(
             .build();
 
         // Set it as the global provider
-        opentelemetry::global::set_tracer_provider(tracer_provider);
+        opentelemetry::global::set_tracer_provider(tracer_provider.clone());
 
         let tracer = opentelemetry::global::tracer("music-manager");
 
         // Get a tracer from the global provider
-        Some(tracing_opentelemetry::layer().with_tracer(tracer))
+        (
+            Some(tracing_opentelemetry::layer().with_tracer(tracer)),
+            Some(tracer_provider),
+        )
     } else {
-        None
+        (None, None)
     };
 
     let fmt_layer = tracing_subscriber::fmt::layer().pretty();
-    let filter_layer = EnvFilter::new(tracing_level);
+    let filter_layer =
+        EnvFilter::try_new(tracing_level).wrap_err("Failed to create tracing filter")?;
 
     if let Some(telemetry_layer) = telemetry_layer {
         tracing_subscriber::registry()
@@ -58,5 +63,5 @@ pub fn init_tracing(
             .init();
     }
 
-    Ok(())
+    Ok(tracer_provider)
 }
